27c1440b5579512a359971153c548d1cd4bbd9df
Guido <guido@lounge.local>
2017-06-07 10:08:58 +0200

    Initial commit

---
+++ callback/lr2tumblr.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> Lightroom-Tumblr Plugin</title>
+</head>
+<body>
+<h1>Plugin Verification</h1>
+<p>Please enter the following code in Lightroom:</p>
+<code style="font-size: 150%">
+<?= $_REQUEST['oauth_verifier'] ?>
+</body>
+</html>
